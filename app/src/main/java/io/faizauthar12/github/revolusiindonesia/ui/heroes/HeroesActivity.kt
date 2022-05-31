b88b0f2522272dd27b4fab61b103bf13c67ac55b
Faiz Authar <faizauthar@gmail.com>
2022-05-31 11:13:55 +0700

    Heroes Activity implement Recycleview

--- app/src/main/java/io/faizauthar12/github/revolusiindonesia/ui/heroes/HeroesActivity.kt
+++ app/src/main/java/io/faizauthar12/github/revolusiindonesia/ui/heroes/HeroesActivity.kt
@@ -2,6 +2,8 @@ package io.faizauthar12.github.revolusiindonesia.ui.heroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.faizauthar12.github.revolusiindonesia.R
 import io.faizauthar12.github.revolusiindonesia.databinding.ActivityHeroesBinding
 
@@ -13,5 +15,21 @@ class HeroesActivity : AppCompatActivity() {
 
         binding = ActivityHeroesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // ViewModel
+        val viewModel = ViewModelProvider(this).get(HeroesViewModel::class.java)
+        val heroes = viewModel.getHeroes()
+
+        // Heroes Adapter
+        val heroesAdapter = HeroesAdapter()
+
+        // Recycleview
+        with(binding.rvHero) {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+            setHasFixedSize(true)
+
+            adapter = heroesAdapter
+        }
     }
 }
