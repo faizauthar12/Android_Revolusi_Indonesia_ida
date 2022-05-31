ca89452aa0c88681e3566cd33622a25da80ecd87
Faiz Authar <faizauthar@gmail.com>
2022-05-31 09:45:46 +0700

    HomeActivity: With Content and viewBinding

--- app/src/main/java/io/faizauthar12/github/revolusiindonesia/ui/home/HomeActivity.kt
+++ app/src/main/java/io/faizauthar12/github/revolusiindonesia/ui/home/HomeActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.faizauthar12.github.revolusiindonesia.R
 import io.faizauthar12.github.revolusiindonesia.databinding.ActivityHomeBinding
+import io.faizauthar12.github.revolusiindonesia.databinding.ContentHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var contentHome: ContentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        contentHome = binding.contentHome
     }
 }
