89ff7b048367903c1229a1bf1b86d706f891460a
Faiz Authar <faizauthar@gmail.com>
2022-05-31 12:08:06 +0700

    utils: Hero Picture fix link

--- app/src/main/java/io/faizauthar12/github/revolusiindonesia/utils/dummy/HeroDummy.kt
+++ app/src/main/java/io/faizauthar12/github/revolusiindonesia/utils/dummy/HeroDummy.kt
@@ -9,7 +9,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Soedirman",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Jenderal_Sudirman.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Jenderal_Sudirman.jpg/565px-Jenderal_Sudirman.jpg",
                 heroDescription = "Jenderal Besar TNI (Anumerta) Raden Soedirman (EYD: Sudirman; 24 Januari 1916 – 29 Januari 1950[a]) adalah seorang perwira tinggi Indonesia pada masa Revolusi Nasional Indonesia. Sebagai panglima besar Tentara Nasional Indonesia pertama, ia adalah sosok yang dihormati di Indonesia. Terlahir dari pasangan rakyat biasa di Purbalingga, Hindia Belanda, Soedirman diadopsi oleh pamannya yang seorang priyayi. Setelah keluarganya pindah ke Cilacap pada tahun 1916, Soedirman tumbuh menjadi seorang siswa rajin; ia sangat aktif dalam kegiatan ekstrakurikuler, termasuk mengikuti program kepanduan yang dijalankan oleh organisasi Islam Muhammadiyah. Saat di sekolah menengah, Soedirman mulai menunjukkan kemampuannya dalam memimpin dan berorganisasi. Soedirman sangat dihormati oleh masyarakat karena ketaatannya pada Islam. Setelah berhenti kuliah keguruan, pada 1936 ia mulai bekerja sebagai seorang guru, dan kemudian menjadi kepala sekolah, di sekolah dasar Muhammadiyah; ia juga aktif dalam kegiatan Muhammadiyah lainnya dan menjadi pemimpin Kelompok Pemuda Muhammadiyah pada tahun 1937. Setelah Jepang menduduki Hindia Belanda pada 1942, Soedirman tetap mengajar. Pada tahun 1944, ia bergabung dengan tentara Pembela Tanah Air (PETA) yang disponsori Jepang, menjabat sebagai komandan batalion di Banyumas. Selama menjabat, Soedirman bersama rekannya sesama prajurit melakukan pemberontakan, hingga kemudian diasingkan ke Bogor."
             )
         )
@@ -17,7 +17,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Ki Hadjar Dewantara",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Ki_Hadjar_Dewantara_Mimbar_Umum_18_October_1949_p2.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/3/3a/Ki_Hadjar_Dewantara_Mimbar_Umum_18_October_1949_p2.jpg",
                 heroDescription = "Raden Mas Soewardi Soerjaningrat (EBI: Suwardi Suryaningrat, sejak 1922 menjadi Ki Hadjar Dewantara, EBI: Ki Hajar Dewantara, beberapa menuliskan bunyi bahasa Jawanya dengan Ki Hajar Dewantoro; 2 Mei 1889 – 26 April 1959; selanjutnya disingkat sebagai \"Soewardi\" atau \"KHD\") adalah aktivis pergerakan kemerdekaan Indonesia, kolumnis, politisi, dan pelopor pendidikan bagi kaum pribumi Indonesia dari zaman penjajahan Belanda. Ia adalah pendiri Perguruan Taman Siswa, suatu lembaga pendidikan yang memberikan kesempatan bagi para pribumi untuk bisa memperoleh hak pendidikan seperti halnya para priyayi maupun orang-orang Belanda."
             )
         )
@@ -25,7 +25,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Sutomo",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Bung_Tomo.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Bung_Tomo.jpg",
                 heroDescription = "Sutomo dilahirkan di Kampung Blauran, Surabaya. Ayahnya bernama Kartawan Tjiptowidjojo, priyayi golongan menengah yang pernah bekerja sebagai pegawai pemerintah, staf perusahaan swasta, asisten kantor pajak, hingga pegawai perusahan ekspor-impor Belanda. Kartawan mengaku mempunyai pertalian darah dengan beberapa pengikut dekat Pangeran Diponegoro."
             )
         )
@@ -33,7 +33,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Soekarno",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Presiden_Sukarno.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/0/01/Presiden_Sukarno.jpg",
                 heroDescription = "Dr. (H.C.) Ir. H. Soekarno1 (ER, EYD: Sukarno, nama lahir: Koesno Sosrodihardjo; 6 Juni 1901 – 21 Juni 1970) adalah Presiden pertama Republik Indonesia yang menjabat pada periode 1945–1967. Ia adalah seorang tokoh perjuangan yang memainkan peranan penting dalam memerdekakan bangsa Indonesia dari penjajahan Belanda. Ia adalah Proklamator Kemerdekaan Indonesia (bersama dengan Mohammad Hatta) yang terjadi pada tanggal 17 Agustus 1945. Soekarno adalah yang pertama kali mencetuskan konsep mengenai Pancasila sebagai dasar negara Indonesia dan ia sendiri yang menamainya."
             )
         )
@@ -41,7 +41,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Gatot Soebroto",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Col_Gatot_Subroto,_Kenang-Kenangan_Pada_Panglima_Besar_Letnan_Djenderal_Soedirman,_p27.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/b/be/Col_Gatot_Subroto%2C_Kenang-Kenangan_Pada_Panglima_Besar_Letnan_Djenderal_Soedirman%2C_p27.jpg",
                 heroDescription = "Setamat pendidikan dasar di Hollandsch-Inlandsche School (HIS), Gatot Subroto tidak melanjutkan ke jenjang yang lebih tinggi, tetapi memilih menjadi pegawai. Namun tak lama kemudian pada tahun 1923 memasuki sekolah militer het Koninklijke Nederlands(ch)-Indische Leger (KNIL) di Magelang. Sempat menjadi sersan kelas II saat dikirim di Padang Panjang selama lima tahun, Gatot Subroto kemudian dikirim ke Sukabumi untuk mengikuti pendidikan lanjutan, pendidikan masose. Gatot Subroto dikenal sebagai tentara yang solider terhadap rakyat kecil meski tengah bekerja sebagai tentara kependudukan Belanda dan Jepang. Ia dianggap contoh seorang pemimpin yang layak diapresiasi berkat jasa-jasanya. Bergabung dengan KNIL membuat Gatot Subroto paham dan mengerti bagaimana seorang tentara harus bertindak."
             )
         )
@@ -49,7 +49,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Mohammad Hatta",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:VP_Hatta.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/3/3b/VP_Hatta.jpg",
                 heroDescription = "Dr. (H.C.) Drs. H. Mohammad Hatta (12 Agustus 1902 – 14 Maret 1980) adalah negarawan dan ekonom Indonesia yang menjabat sebagai Wakil Presiden Indonesia pertama. Ia bersama Soekarno memainkan peranan sentral dalam perjuangan kemerdekaan Indonesia dari penjajahan Belanda sekaligus memproklamirkannya pada 17 Agustus 1945. Ia pernah menjabat sebagai Perdana Menteri dalam Kabinet Hatta I, Hatta II, dan RIS. Pada 1956, ia mundur dari jabatan wakil presiden."
             )
         )
@@ -57,7 +57,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Ahmad Yani",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Ahmad_Yani.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/3/3f/Ahmad_Yani.jpg",
                 heroDescription = "Jenderal TNI (Anumerta) Ahmad Yani (juga dieja A. Yani, Achmad Yani; (19 Juni 1922 – 1 Oktober 1965) adalah Menteri/Panglima Angkatan Darat (KSAD) yang merupakan salah satu Pahlawan Revolusi yang gugur menjadi korban tragedi Gerakan 30 September dibunuh oleh anggota Gerakan 30 September saat mencoba untuk menculik dia dari rumahnya."
             )
         )
@@ -65,7 +65,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Soepomo",
-                heroPicture = "https://en.wikipedia.org/wiki/File:Supomo.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/a/a6/Supomo.jpg",
                 heroDescription = "Prof. Dr. Mr. Soepomo (Ejaan Soewandi: Supomo; 22 Januari 1903 – 12 September 1958) adalah seorang pahlawan nasional Indonesia. Soepomo dikenal sebagai perancang Undang-undang Dasar 1945, bersama dengan Muhammad Yamin dan Soekarno."
             )
         )
@@ -73,7 +73,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Tan Malaka",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:TanMalaka_DariPendjara_ed3.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/f/f7/TanMalaka_DariPendjara_ed3.jpg",
                 heroDescription = "Tan Malaka atau Ibrahim Gelar Datuk Sutan Malaka (2 Juni 1897 – 21 Februari 1949) adalah seorang pejuang kemerdekaan Indonesia,[1] juga pendiri Partai Murba, dan merupakan salah satu Pahlawan Nasional Indonesia."
             )
         )
@@ -81,7 +81,7 @@ object HeroDummy {
         heroes.add(
             HeroEntity(
                 heroName = "Ahmad Dahlan",
-                heroPicture = "https://id.wikipedia.org/wiki/Berkas:Ahmad_Dahlan.jpg",
+                heroPicture = "https://upload.wikimedia.org/wikipedia/commons/8/87/Ahmad_Dahlan.jpg",
                 heroDescription = "Kiai Haji Ahmad Dahlan atau Muhammad Darwis (1 Agustus 1868 – 23 Februari 1923) adalah seorang Ulama Besar bergelar Pahlawan Nasional Indonesia yang merupakan pendiri Muhammadiyah. Dia adalah putra keempat dari tujuh bersaudara dari keluarga K.H. Abu Bakar. KH Abu Bakar adalah seorang ulama dan khatib terkemuka di Masjid Besar Kasultanan Yogyakarta pada masa itu, dan ibu dari K.H. Ahmad Dahlan adalah puteri dari H. Ibrahim yang juga menjabat penghulu Kesultanan Ngayogyakarta Hadiningrat pada masa itu."
             )
         )
