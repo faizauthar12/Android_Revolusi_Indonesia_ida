d9a8eb1045255f016d6759e537a53a53154ac2c7
Faiz Authar <faizauthar@gmail.com>
2022-06-02 06:10:02 +0700

    HeroEntitiy: Implement Parcelable

--- app/src/main/java/io/faizauthar12/github/revolusiindonesia/data/HeroEntity.kt
+++ app/src/main/java/io/faizauthar12/github/revolusiindonesia/data/HeroEntity.kt
@@ -1,7 +1,11 @@
 package io.faizauthar12.github.revolusiindonesia.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class HeroEntity(
     var heroName: String,
     var heroPicture: String,
     var heroDescription: String
-)
+) : Parcelable
