f2030dfd0539e590eb3498c9e6eb1a161da8a249
Faiz Authar <faizauthar@gmail.com>
2022-06-02 06:16:42 +0700

    DetailHeroesActivity

---
+++ app/src/main/java/io/faizauthar12/github/revolusiindonesia/ui/heroes/detail/DetailHeroesActivity.kt
@@ -0,0 +1,45 @@
+package io.faizauthar12.github.revolusiindonesia.ui.heroes.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import io.faizauthar12.github.revolusiindonesia.data.HeroEntity
+import io.faizauthar12.github.revolusiindonesia.databinding.ActivityDetailHeroesBinding
+import io.faizauthar12.github.revolusiindonesia.databinding.ContentDetailHeroesBinding
+
+class DetailHeroesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailHeroesBinding
+    private lateinit var contentDetail: ContentDetailHeroesBinding
+
+    private var data: HeroEntity? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailHeroesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        contentDetail = binding.contentDetailHeroes
+        setSupportActionBar(binding.toolbar)
+
+        val hero = intent.extras
+        if (hero != null) {
+            data = intent.getParcelableExtra(EXTRA_HERO)
+            data?.let {
+                populateDetailHeroes(it)
+            }
+        }
+    }
+
+    private fun populateDetailHeroes(heroEntity: HeroEntity) {
+        contentDetail.tvHeroName.text = heroEntity.heroName
+        contentDetail.tvHeroDescription.text = heroEntity.heroDescription
+        Glide.with(this)
+            .load(heroEntity.heroPicture)
+            .into(contentDetail.ivHeroes)
+    }
+
+    companion object {
+        const val EXTRA_HERO = "extra_hero"
+    }
+}
